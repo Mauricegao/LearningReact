940ad3f11bc747870626d2d802d78d7c6faae95e
Mauricegao <yunzhi@ualberta.ca>
2019-04-23 15:53:49 -0600

    Enabled language toggle

--- translate/src/component/Button.js
+++ translate/src/component/Button.js
@@ -5,8 +5,8 @@ class Button extends React.Component {
   static contextType = LanguageContext;
 
   render() {
-    console.log(this.context);
-    return <button className="ui primary button">Submit</button>;
+    const text = this.context === "english" ? "Submit" : "Soumettre";
+    return <button className="ui primary button">{text}</button>;
   }
 }
 
--- translate/src/contexts/LanguageContext.js
+++ translate/src/contexts/LanguageContext.js
@@ -1,4 +1,7 @@
 import React from "react";
 
+const context = React.createContext("english");
 
-export default React.createContext("english");
+console.log(context);
+
+export default context;
